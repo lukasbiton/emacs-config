08603fa46c92c66bcca4f6810083d4344acefb6f
LB <github.com.skimmer184@passmail.net>
2025-12-18 00:16:59 +0000

    Update init.el

--- init.el
+++ init.el
@@ -1,18 +1,26 @@
-;;; package  --- Summary
-;; personalised init.el
-
-;;; Commentary:
-;; implements some quality of life improvements
-;; uses eglot for the language server protocol
-;; install vterm (with external installs needed)
-;; uses counsel and ivy for better navigation
+;; Lightweight emacs set-up
+;; Upgraded completion and search
+;; LSP for python and C using uv/jedi and ccls
 
 (setq-default
  inhibit-startup-screen t               ; Disable start-up screen
  inhibit-startup-message t              ; Disable startup message
  inhibit-startup-echo-area-message t    ; Disable initial echo message
  initial-scratch-message ""             ; Empty the initial *scratch* buffer
- initial-buffer-choice t)               ; Open *scratch* buffer at init
+ initial-buffer-choice t                ; Open *scratch* buffer at init
+ ring-bell-function 'ignore             ; Stop bell sounds
+)
+
+(scroll-bar-mode -1) ; Disable scroll bar
+(tool-bar-mode -1) ; Disable toolbar
+(tooltip-mode -1) ; Disable tooltips
+(menu-bar-mode -1) ; Disable the menu bar
+(add-to-list 'default-frame-alist '(undecorated . t)) ; Remove top bar
+(winner-mode t) ; Allow to undo window configurations
+(savehist-mode t) ; Save minibuffer histories
+(recentf-mode t) ; Keeps track of recently visited files
+(which-function-mode 1) ; Display in mode line the current function
+(windmove-default-keybindings) ; Move buffers with S-<arrow key>
 
 ;; Save ~ files and other backups all together
 (setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
@@ -23,44 +31,40 @@
   kept-old-versions 5    ; and how many of the old
   )
 
-(scroll-bar-mode -1) ; Disable scroll bar
-(tool-bar-mode -1) ; Disable toolbar
-(tooltip-mode -1) ; Disable tooltips
-(menu-bar-mode -1) ; Disable the menu bar
-(add-to-list 'default-frame-alist '(undecorated . t))
-
-(winner-mode t) ; Allow to undo window configurations
-
 ;; Initialize package sources
 (require 'package)
 
+;; Set archives
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
  			 ("org" . "https://orgmode.org/elpa/")
  			 ("elpa" . "https://elpa.gnu.org/packages/")))
 
-;; Allows using older versions of packages for greater
-;; compatibility
+;; Allows using older versions of packages for greater compatibility
 (add-to-list 'package-archives
-	     (cons "gnu-devel" "https://elpa.gnu.org/devel/")
- 	     t)
+	     (cons "gnu-devel" "https://elpa.gnu.org/devel/") t)
 
 ;; Initialize use-package on non-Linux platform
 (unless (package-installed-p 'use-package) ; use-package not installed by default
    (package-install 'use-package))
-
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-(global-display-line-numbers-mode t) ; Activate display of line number
+;; To modify modeline display of minor modes
+(use-package delight
+  :ensure t)
 
-;; Make it pretty
+;; Themes
 (use-package ef-themes
   :ensure t
   :config
   ;; Use ef-themes-toggle to cycle through these
-  (setq ef-themes-to-toggle '(ef-day ef-dream))
-  (ef-themes-select 'ef-dream))
+  (setq ef-themes-to-toggle '(ef-day ef-dream)))
+(use-package doric-themes
+  :ensure t
+  :config
+  (doric-themes-select 'doric-obsidian))
 
+;; Make the UI slick
 (use-package spacious-padding
   :ensure t
   :config
@@ -71,6 +75,21 @@
          :header-line-inactive spacious-padding-line-inactive)))
 (spacious-padding-mode 1)
 
+;; Custom mode line
+(setopt mode-line-format
+        '("%*"
+          vc-mode
+	  mode-line-front-space
+          mode-line-buffer-identification
+	  (:eval (which-function))
+          mode-line-format-right-align
+	  mode-line-modes
+          project-mode-line
+	  project-mode-line-format
+	  " "
+	  buffer-file-name
+	  ))
+
 ;; Disable line numbers for some modes
 (dolist (mode '(term-mode-hook
 		eshell-mode-hook
@@ -80,6 +99,7 @@
 		magit-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
+;; Nice vertical menus
 (use-package vertico
   :init (vertico-mode)
   :config
@@ -113,7 +133,7 @@
 (use-package embark-consult
   :ensure t)
 
-;; Suggests keybindings
+;; Act on selection
 (use-package embark
   :ensure t
   :bind
@@ -157,6 +177,7 @@
   :hook (completion-list-mode . consult-preview-at-point-mode)
   )
 
+;; Autocompletion
 (use-package corfu
   ;; Optional customizations
   :custom
@@ -174,20 +195,27 @@
   :init
   (global-corfu-mode))
 
+;; Pretty icons
 (use-package nerd-icons)
 
+;; Pretty icons when using completion
 (use-package nerd-icons-corfu
   :ensure t
   :init
   (add-to-list 'corfu-margin-formatters #'nerd-icons-corfu-formatter))
 
-;; Show pretty icons in dired mode too
+;; Show pretty icons in dired
 (use-package nerd-icons-dired
   :hook
   (dired-mode . nerd-icons-dired-mode))
 
-;; Gives more useful completion when you start typing a command
+(use-package eldoc
+  :delight eldoc-mode
+  :ensure t)
+
+;; Show potential commands
 (use-package which-key
+  :delight which-key-mode
   :init (which-key-mode)
   :diminish which-key-mode
   :config
@@ -209,36 +237,49 @@
   :config
   (setq magit-save-repository-buffers nil))
 
-
+;; Treesit install paths
 (when (boundp 'treesit-extra-load-path)
   (add-to-list 'treesit-extra-load-path "/usr/local/lib/")
   (add-to-list 'treesit-extra-load-path "~/.local/lib/"))
-
+;; Treesit tested versions
 (setq treesit-language-source-alist
       '((c "git@github.com:tree-sitter/tree-sitter-c.git" "v0.20.7")
 	(cpp "git@github.com:tree-sitter/tree-sitter-cpp.git" "v0.23.2")
 	(python "git@github.com:tree-sitter/tree-sitter-python.git" "v0.23.3")))
-
+;; Use treesiter modes
 (setq major-mode-remap-alist
       '((python-mode . python-ts-mode)
 	(c-mode . c-ts-mode)
 	(cpp-mode . cpp-ts-mode)))
 
+;; So I can use my own indentation
+(setq c-syntactic-indentation nil)
+
+;; Language server protocol
+;; For uv, see online installation instructions
+;; Need to separately install ccls, sudo apt install works
 (use-package eglot
   :ensure t
-   :hook ((python-ts-mode . eglot-ensure)
- 	 (python-ts-mode . electric-pair-mode)
- 	 (python-ts-mode . electric-quote-mode)
- 	 (python-ts-mode . electric-indent-mode)
- 	 (c-ts-mode . eglot-ensure)
- 	 (c-ts-mode . electric-pair-mode)
- 	 (c-ts-mode . electric-quote-mode)
- 	 (c-ts-mode . electric-indent-mode)))
-
-;; Need to separately install ccls, sudo apt install works
-(with-eval-after-load 'eglot
+  :hook ((python-ts-mode . eglot-ensure)
+	 (c-ts-mode . eglot-ensure))
+  :config
+  (setq eglot-autoshutdown t)
   (add-to-list 'eglot-server-programs
-	       '(c-ts-mode . ("ccls"))))
+	       '(c-ts-mode . "ccls")
+	       '(python-ts-mode . ("uv" "tool" "run" "jedi-language-server")))
+  )
+
+(use-package autorevert
+  :delight auto-revert-mode
+  :ensure t)
+
+(use-package flymake
+  :delight flymake-mode
+  :ensure t)
+
+(use-package flymake-ruff
+  :ensure t
+  :hook (python-ts-mode . flymake-ruff-load))
 
 ;; For scheme
 (use-package geiser-mit
@@ -269,22 +310,14 @@
      (lambda (face) (set-face-attribute face nil :height (* new-size 10)))
      faces)))
 
-(defun toggle-theme ()
-  (interactive)
-  (ef-themes-toggle)
-  (set-font-size 9))
-
 ;; Terminal replacement
 (use-package vterm
   :ensure t
   :bind (("C-c v" . vterm)))
 
 (use-package envrc
+  :delight envrc-mode
   :hook (after-init . envrc-global-mode))
-
-(use-package ace-window
-  :ensure t
-  :bind ("M-o" . ace-window))
 
 (use-package vundo
   :ensure t)
@@ -315,11 +348,21 @@
 (keymap-global-set "C-S-k" 'kill-sexp)
 (keymap-global-set "C-S-SPC" 'mark-sexp)
 
-;; Save minbuffer histories
-(savehist-mode 1)
+;; Command to resize a window to ~60% of the overall size
+(defun window-resize-to-66-percent ()
+  (interactive)
+  (window-resize nil (- (truncate (* 0.66666 (frame-width))) (window-width)) t))
 
-;; Keeps track of recently visited files
-(recentf-mode 1)
+;; ANSI colors when using M-x compile
+(use-package fancy-compilation
+  :commands (fancy-compilation-mode))
+
+(with-eval-after-load 'compile
+  (fancy-compilation-mode))
+
+;; Make music!
+(use-package tidal
+  :ensure t)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -327,10 +370,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(ace-window corfu csv-mode ef-themes embark-consult envrc geiser-mit
-		magit marginalia markdown-mode nerd-icons-corfu
-		nerd-icons-dired orderless org2blog spacious-padding
-		tree-sitter treesit vertico vterm vundo)))
+   '(auto-revert corfu csv-mode delight doric-themes ef-themes eglot
+		 embark-consult envrc fancy-compilation flymake-ruff
+		 geiser-mit magit marginalia markdown-mode
+		 nerd-icons-corfu nerd-icons-dired orderless org2blog
+		 spacious-padding tidal vertico vterm vundo)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
